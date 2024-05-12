f76e3cb19ad16715aa2397a22c4e12a1cbdad14c
Sindre0 <143394520+Sindre0@users.noreply.github.com>
2024-05-12 19:06:16 +0200

    satt opp mer routing og planlegging av komponenter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import Layout from './components/Layout'
 import Home from './components/Home'
+import Teams from './components/Teams'
+import Type from './components/Type'
+import SearchResults from './components/SearchResult'
+import Pokemon from './components/Pokemon'
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -10,7 +14,11 @@ function App() {
     <>
     <Layout>
       <Routes>
-        <Route index element={<Home />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/teams" element={<Teams />} />
+        <Route path="/:type" element={<Type />} />
+        <Route path="/searchresults/:pokemon" element={<SearchResults />} />
+        <Route path="/pokemons/:pokemon" element={<Pokemon />} />
       </Routes>
     </Layout>
     </>
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,11 @@
+import PokeCard from './PokeCard'
+
 export default function Home(){
     return(
         <>
         <section>
             <h1>MAIN POKEMONS</h1>
+            <PokeCard limit={9} />
         </section>
         <section>
             <h2>TYPES</h2>
