89fe81a23ebc9b49740bafd54db498425743a019
Sindre0 <143394520+Sindre0@users.noreply.github.com>
2024-05-14 15:22:35 +0200

    fått til api-kall for pokemon på hjemmesiden
    TODO: få de listet opp på siden

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,21 @@
 import PokeCard from './PokeCard'
+import { useState, useEffect } from 'react'
 
 export default function Home(){
+    const [pokemon, setPokemon] = useState([])
+
+    useEffect(() => {
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=9')
+          .then(response => response.json())
+          .then(data => setPokemon(data.results))
+      }, [])
+
+      console.log(pokemon)
     return(
         <>
         <section>
             <h1>MAIN POKEMONS</h1>
-            <PokeCard limit={9} />
+            
         </section>
         <section>
             <h2>TYPES</h2>
