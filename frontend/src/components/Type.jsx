637d6be20ab0c8305e5e18b49839b94348c898dd
Sondre Jønsson <sondrej@hiof.no>
2024-05-15 13:45:24 +0200

    Lagt inn løsning for henting av type data fra API

--- frontend/src/components/Type.jsx
+++ frontend/src/components/Type.jsx
@@ -1,3 +1,19 @@
-export default function Type(){
-    
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+export default function Type() {
+  const { type } = useParams()
+  const [pokemon, setPokemon] = useState([])
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/type/${type}`)
+      .then(response => response.json())
+      .then(data => setPokemon(data.pokemon))
+  }, [type])
+
+  return (
+    <>
+
+    </>
+  )
 }
