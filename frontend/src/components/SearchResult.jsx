fd08af1470523671be36b149e112255467a9475c
Sindre0 <143394520+Sindre0@users.noreply.github.com>
2024-05-16 18:19:44 +0200

    fått til api-kall og visning av søkeresultat
    alt fungerer ganske bra. api-kallet er basert på url-en, og det kjøren en if-test ut ifra kallet. det eneste jeg har merket som er kjipt er at hvis man skriver stor bokstav, så vil søket feile selvom man skriver navnet på pokemon riktig. Kan løses ved input restriksjoner, eller gjøre det om til lowercase automatisk.

--- frontend/src/components/SearchResult.jsx
+++ frontend/src/components/SearchResult.jsx
@@ -1,3 +1,44 @@
-export default function SearchResults(){
-    
+import { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
+
+export default function SearchResult() {
+  const { pokemon } = useParams();
+  const [pokemonData, setPokemonData] = useState(null);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    // Tilbakestill error og pokemonData når pokemon endrer seg
+    setError(false)
+    setPokemonData(null)
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+      .then(response => response.json())
+      .then(data => {
+        if (data) {
+          setPokemonData(data)
+        } else {
+          setError(true)
+        }
+      })
+      .catch(() => setError(true))
+  }, [pokemon])
+
+
+
+  return (
+    <div>
+      {error ? (
+        <p>Finner ikke noen pokemon med navnet "{pokemon}" </p>
+      ) : pokemonData ? (
+        <div>
+          <h1>
+            <Link to={`/pokemons/${pokemonData.name}`}>{pokemonData.name}</Link>
+          </h1>
+          <img src={pokemonData.sprites.front_default} alt={pokemonData.name} />
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  )
 }
