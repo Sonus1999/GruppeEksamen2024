bcb6d8977af77588533f2220d103c52236dd34bd
Sindre0 <143394520+Sindre0@users.noreply.github.com>
2024-05-16 14:58:04 +0200

    Fått til søkefunksjon
    Søkefunksjon fungerer nå og oppdaterer URL. Vi kan da bruke slug for å gjøre søket i searchresult-komponentet.
    Tar forbehold om at søkefunksjonen ikke er perfekt, men kan fikses senere

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,7 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 
 export default function Layout({children}){
+    const [search, setSearch] = useState('')
+    const navigate = useNavigate()
+
+    const handleSubmit = (e)=> {
+        e.preventDefault()
+        navigate(`/searchresults/${search}`)
+    }
+    
+    
     return(
         <>
         <header>
@@ -11,6 +21,15 @@ export default function Layout({children}){
                     <li><Link to="/teams">TEAMS</Link></li>
                 </ul>
             </nav>
+            <form onSubmit={handleSubmit}>
+                <input
+                type="text"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                placeholder="Search for a Pokemon"
+            />
+          <button type="submit">Search</button>
+        </form>
         </header>
         <main>
         {children}
