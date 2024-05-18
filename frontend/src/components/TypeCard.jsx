5eebf3a0dabb8f412bbc477df1aa762ad976be9c
Sindre0 <143394520+Sindre0@users.noreply.github.com>
2024-05-18 19:56:17 +0200

    lagt til fargekodene og en if-test
    lagt til fargekoder for å se at strukturen fungerer på typecards.
    også lagt til en if-test pga stavefeil i githubrepoet UIN24_eksamen

--- frontend/src/components/TypeCard.jsx
+++ frontend/src/components/TypeCard.jsx
@@ -6,7 +6,8 @@ export default function TypeCard({name}){
             <p>
             <Link to={`/${name}`}>{name}</Link>
             </p>
-            <img src={`https://raw.githubusercontent.com/ackarlse/UIN24_eksamen/master/ressurser/type%20symboler/${name}.png`}></img>
+            {name == "poison" ? <img src={`https://raw.githubusercontent.com/ackarlse/UIN24_eksamen/master/ressurser/type%20symboler/poisen.png`}></img>
+            : <img src={`https://raw.githubusercontent.com/ackarlse/UIN24_eksamen/master/ressurser/type%20symboler/${name}.png`}></img>}
         </article>
         </>
     )
